4bf6cfa87e657bcd5a9c119c1422a16c3f066d1e
DevVictor19 <antoniovictor12@live.com>
2024-02-04 21:47:35 -0300

    feat: project initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
+@Module({})
 export class AppModule {}
