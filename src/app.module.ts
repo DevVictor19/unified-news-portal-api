0033c0d9b0031a71964ac82b88d87127cedc23d0
DevVictor19 <antoniovictor12@live.com>
2024-03-22 15:46:11 -0300

    feat: add MongooseModule configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import environment from './config/environment.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [environment] })],
+  imports: [
+    ConfigModule.forRoot({ load: [environment] }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>('database.host'),
+      }),
+      imports: [ConfigModule],
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
