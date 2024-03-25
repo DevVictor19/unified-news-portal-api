1618e7638b51c6ecb452b7b2126fbb6b14b0b9a1
DevVictor19 <antoniovictor12@live.com>
2024-03-24 22:26:11 -0300

    feat: add more variables on environment config

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -1,8 +1,14 @@
 export default () => ({
   server: {
     port: Number(process.env.SERVER_PORT),
+    url: process.env.SERVER_URL,
   },
   database: {
     host: process.env.DB_HOST,
+  },
+  mail: {
+    email: process.env.MAIL_EMAIL,
+    name: process.env.MAIL_NAME,
+    password: process.env.MAIL_PASSWORD,
   },
 });
