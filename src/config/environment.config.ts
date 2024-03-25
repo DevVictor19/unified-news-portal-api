7e2d35927455f16d0bdafb6014ab80ba9a95103d
DevVictor19 <antoniovictor12@live.com>
2024-03-25 20:09:39 -0300

    feat: add secret_key on environment config

--- src/config/environment.config.ts
+++ src/config/environment.config.ts
@@ -2,6 +2,7 @@ export default () => ({
   server: {
     port: Number(process.env.SERVER_PORT),
     url: process.env.SERVER_URL,
+    secret_key: process.env.SERVER_SECRET_KEY,
   },
   database: {
     host: process.env.DB_HOST,
