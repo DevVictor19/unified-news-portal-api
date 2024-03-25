487a247ada30bb7df725aa94470787877a74d041
DevVictor19 <antoniovictor12@live.com>
2024-03-25 20:13:47 -0300

    feat(users): add handlebars-template-engine-provider

--- src/modules/users/providers/implementations/handlebars-template-engine.provider.ts
+++ src/modules/users/providers/implementations/handlebars-template-engine.provider.ts
@@ -1,5 +1,6 @@
 import { compile } from 'handlebars';
 import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
 
 import { ITemplateEngineProvider } from '../template-engine-provider.interface';
 
@@ -7,10 +8,11 @@ export class HandleBarsTemplateEngineProvider
   implements ITemplateEngineProvider
 {
   compile(
-    templatePath: string,
+    fileName: string,
     variables?: { [key: string]: string | object } | undefined,
   ): string {
-    const html = readFileSync(templatePath).toString();
+    const workdir = process.cwd();
+    const html = readFileSync(join(workdir, 'templates', fileName)).toString();
 
     const template = compile(html);
 
