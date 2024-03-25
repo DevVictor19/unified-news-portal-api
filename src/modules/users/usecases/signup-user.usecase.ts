5fd71fc1f5e4b10653d33df7e347dfc1b744e98b
DevVictor19 <antoniovictor12@live.com>
2024-03-25 20:14:21 -0300

    feat(users): send verficiation email on signup

--- src/modules/users/usecases/signup-user.usecase.ts
+++ src/modules/users/usecases/signup-user.usecase.ts
@@ -2,6 +2,9 @@ import { BadRequestException } from '@nestjs/common';
 
 import { UsersFactory } from '../entities/users.factory';
 import { IHashProvider } from '../providers/hash-provider.interface';
+import { IJwtProvider } from '../providers/jwt-provider.interface';
+import { IMailProvider } from '../providers/mail-provider.interface';
+import { ITemplateEngineProvider } from '../providers/template-engine-provider.interface';
 import { IUsersRepository } from '../repositories/users-repository.interface';
 
 import { IBaseUseCase } from '@/common/abstractions/usecases/base-usecase.abstraction';
@@ -19,6 +22,10 @@ export class SignupUserUseCase implements IBaseUseCase<Input, Output> {
     private usersReporitory: IUsersRepository,
     private usersFactory: UsersFactory,
     private hashProvider: IHashProvider,
+    private templateProvider: ITemplateEngineProvider,
+    private jwtProvider: IJwtProvider,
+    private mailProvider: IMailProvider,
+    private serverUrl: string,
   ) {}
 
   async execute(input: Input): Promise<Output> {
@@ -27,6 +34,26 @@ export class SignupUserUseCase implements IBaseUseCase<Input, Output> {
     if (existingUser) {
       throw new BadRequestException('Email already in use');
     }
+
+    const token = this.jwtProvider.sign({
+      payload: { email: input.email },
+      expiresIn: '2h',
+    });
+
+    const verifyEmailUrl = `${this.serverUrl}/users/verify?token=${token}`;
+
+    const html = this.templateProvider.compile('email-verification.hbs', {
+      link: verifyEmailUrl,
+    });
+
+    this.mailProvider.sendMail({
+      body: html,
+      subject: 'Verificação de Email',
+      to: {
+        email: input.email,
+        name: input.name,
+      },
+    });
 
     const hashedPassword = await this.hashProvider.generateHash(input.password);
 
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule, getModelToken } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -6,6 +7,12 @@ import { User, UserSchema } from './entities/users.entity';
 import { UsersFactory } from './entities/users.factory';
 import { IHashProvider } from './providers/hash-provider.interface';
 import { BcryptHashProvider } from './providers/implementations/bcrypt-hash.provider';
+import { HandleBarsTemplateEngineProvider } from './providers/implementations/handlebars-template-engine.provider';
+import { JsonWebTokenProvider } from './providers/implementations/jsonwebtoken.provider';
+import { NodeMailerMailProvider } from './providers/implementations/nodemailer-mail.provider';
+import { IJwtProvider } from './providers/jwt-provider.interface';
+import { IMailProvider } from './providers/mail-provider.interface';
+import { ITemplateEngineProvider } from './providers/template-engine-provider.interface';
 import { UsersMongoRepository } from './repositories/implementations/users-mongo.repository';
 import { IUsersRepository } from './repositories/users-repository.interface';
 import { SignupUserUseCase } from './usecases';
@@ -14,6 +21,7 @@ import { UsersController } from './users.controller';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    ConfigModule,
   ],
   controllers: [UsersController],
   providers: [
@@ -33,19 +41,60 @@ import { UsersController } from './users.controller';
       useClass: BcryptHashProvider,
     },
     {
+      provide: 'TemplateEngineProvider',
+      useClass: HandleBarsTemplateEngineProvider,
+    },
+    {
+      provide: 'JwtProvider',
+      useFactory: (configService: ConfigService) => {
+        return new JsonWebTokenProvider(
+          configService.getOrThrow<string>('server.secret_key'),
+        );
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: 'MailProvider',
+      useFactory: (configService: ConfigService) => {
+        const mailUser = configService.getOrThrow<string>('mail.email');
+        const mailName = configService.getOrThrow<string>('mail.name');
+        const mailPassword = configService.getOrThrow<string>('mail.password');
+        return new NodeMailerMailProvider(mailUser, mailName, mailPassword);
+      },
+      inject: [ConfigService],
+    },
+    {
       provide: SignupUserUseCase,
       useFactory: (
         usersReporitory: IUsersRepository,
         usersFactory: UsersFactory,
         hashProvider: IHashProvider,
+        templateProvider: ITemplateEngineProvider,
+        jwtProvider: IJwtProvider,
+        mailProvider: IMailProvider,
+        configService: ConfigService,
       ) => {
+        const serverUrl = configService.getOrThrow<string>('server.url');
+
         return new SignupUserUseCase(
           usersReporitory,
           usersFactory,
           hashProvider,
+          templateProvider,
+          jwtProvider,
+          mailProvider,
+          serverUrl,
         );
       },
-      inject: ['UsersRepository', UsersFactory, 'HashProvider'],
+      inject: [
+        'UsersRepository',
+        UsersFactory,
+        'HashProvider',
+        'TemplateEngineProvider',
+        'JwtProvider',
+        'MailProvider',
+        ConfigService,
+      ],
     },
   ],
 })
