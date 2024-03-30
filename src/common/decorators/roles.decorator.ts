2cda6fcf702fb241f899651480b0b005ced8d31d
DevVictor19 <antoniovictor12@live.com>
2024-03-30 18:33:44 -0300

    feat: add roles decorator

---
+++ src/common/decorators/roles.decorator.ts
@@ -0,0 +1,36 @@
+import { SetMetadata } from '@nestjs/common';
+
+import { ROLES } from '../enums/roles.enum';
+
+export const ROLES_KEY = 'roles';
+
+export const Roles = (...roles: ROLES[]) => SetMetadata(ROLES_KEY, roles);
+
+export const AdminRoute = () => Roles(ROLES.ADMIN);
+
+export const CoordinatorRoute = () => Roles(ROLES.ADMIN, ROLES.COORDINATOR);
+
+export const TeacherRoute = () =>
+  Roles(ROLES.ADMIN, ROLES.COORDINATOR, ROLES.TEACHER);
+
+export const LeaderRoute = () =>
+  Roles(ROLES.ADMIN, ROLES.COORDINATOR, ROLES.TEACHER, ROLES.LEADER);
+
+export const ViceLeaderRoute = () =>
+  Roles(
+    ROLES.ADMIN,
+    ROLES.COORDINATOR,
+    ROLES.TEACHER,
+    ROLES.LEADER,
+    ROLES.VICE_LEADER,
+  );
+
+export const StudentRoute = () =>
+  Roles(
+    ROLES.ADMIN,
+    ROLES.COORDINATOR,
+    ROLES.TEACHER,
+    ROLES.LEADER,
+    ROLES.VICE_LEADER,
+    ROLES.STUDENT,
+  );
