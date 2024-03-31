1090e686782499d71f1894148c3d79055980808d
DevVictor19 <antoniovictor12@live.com>
2024-03-31 19:01:02 -0300

    feat(subjects): initialize module providers

--- src/modules/subjects/subjects.module.ts
+++ src/modules/subjects/subjects.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule, getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
+import { Subject, SubjectSchema } from './entities/subjects.entity';
+import { SubjectsFactory } from './entities/subjects.factory';
+import { SubjectsMongoRepository } from './repositories/mongo/subjects-mongo.repository';
+import { SUBJECTS_REPOSITORY } from './repositories/subjects-repository.constants';
 import { SubjectsController } from './subjects.controller';
 
 @Module({
   controllers: [SubjectsController],
+  imports: [
+    MongooseModule.forFeature([{ name: Subject.name, schema: SubjectSchema }]),
+  ],
+  providers: [
+    {
+      provide: SUBJECTS_REPOSITORY,
+      useFactory: (subjectModel: Model<Subject>) => {
+        return new SubjectsMongoRepository(subjectModel);
+      },
+      inject: [getModelToken(Subject.name)],
+    },
+    {
+      provide: SubjectsFactory,
+      useClass: SubjectsFactory,
+    },
+  ],
 })
 export class SubjectsModule {}
