60f91a02e1d2969533a25d88f733d0bc3b5b8599
DevVictor19 <antoniovictor12@live.com>
2024-04-06 16:06:47 -0300

    feat(classes): implement controller layer

---
+++ src/modules/classes/dtos/create-classes.dto.ts
@@ -0,0 +1,9 @@
+import { Transform } from 'class-transformer';
+import { IsString, Length } from 'class-validator';
+
+export class CreateClassesDto {
+  @IsString()
+  @Length(2, 25)
+  @Transform(({ value }) => value?.trim())
+  name: string;
+}
