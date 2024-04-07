374674f8e40579136e3b42ee780b418d5535c3fb
DevVictor19 <antoniovictor12@live.com>
2024-04-07 17:20:58 -0300

    feat(categories) add factory on mapper

--- src/modules/categories/database/models/mongo/caregories-mongo-model.mapper.ts
+++ src/modules/categories/database/models/mongo/caregories-mongo-model.mapper.ts
@@ -1,6 +1,7 @@
 import { CategoryMongoEntity } from './categories-mongo.model';
 
 import { CategoryEntity } from '@/modules/categories/entities/categories.entity';
+import { CategoryEntityFactory } from '@/modules/categories/entities/categories.factory';
 
 export class CategoryMongoEntityMapper {
   static toMongoEntity(entity: CategoryEntity): CategoryMongoEntity {
@@ -12,10 +13,10 @@ export class CategoryMongoEntityMapper {
   }
 
   static toDomainEntity(entity: CategoryMongoEntity): CategoryEntity {
-    return {
+    return new CategoryEntityFactory().create({
       id: entity._id,
       created_at: entity.created_at,
       name: entity.name,
-    };
+    });
   }
 }
