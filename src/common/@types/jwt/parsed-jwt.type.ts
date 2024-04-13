d3a0a49e778fb9cc3a80f8dad947aa006170f06e
DevVictor19 <antoniovictor12@live.com>
2024-04-13 14:56:23 -0300

    fix: import jwtPayload from correct path

--- src/common/@types/jwt/parsed-jwt.type.ts
+++ src/common/@types/jwt/parsed-jwt.type.ts
@@ -1,3 +1,3 @@
-import { JwtPayload } from '@/modules/common/jwt/providers/jwt/jwt-provider.interface';
+import { JwtPayload } from '@/modules/common/jwt/jwt-provider.interface';
 
 export type ParsedJwt<T extends object> = T & JwtPayload;
