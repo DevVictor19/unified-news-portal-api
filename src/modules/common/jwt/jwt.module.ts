354ea49cdb53977212b7c1f33829783a431d6ff9
DevVictor19 <antoniovictor12@live.com>
2024-04-13 21:20:44 -0300

    feat: import EnvConfigModule

--- src/modules/common/jwt/jwt.module.ts
+++ src/modules/common/jwt/jwt.module.ts
@@ -2,9 +2,11 @@ import { Global, Module } from '@nestjs/common';
 
 import { IJwtProvider } from './jwt-provider.interface';
 import { JwtProvider } from './jwt.provider';
+import { EnvConfigModule } from '../env-config/env-config.module';
 
 @Global()
 @Module({
+  imports: [EnvConfigModule],
   providers: [
     {
       provide: IJwtProvider,
