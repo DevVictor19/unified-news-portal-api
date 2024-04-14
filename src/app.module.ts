8e0cdb02dca586ffcb75d8103c4cff21b3b32182
DevVictor19 <antoniovictor12@live.com>
2024-04-13 21:22:11 -0300

    fix: import new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 
-import environment from './config/environment.config';
 import { CategoriesModule } from './modules/categories/categories.module';
 import { ClassesModule } from './modules/classes/classes.module';
+import { DatabaseModule } from './modules/common/database/database.module';
+import { EnvConfigModule } from './modules/common/env-config/env-config.module';
 import { JwtModule } from './modules/common/jwt/jwt.module';
 import { CoursesModule } from './modules/courses/courses.module';
 import { PostTypesModule } from './modules/post-types/post-types.module';
@@ -13,15 +12,8 @@ import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ load: [environment], isGlobal: true }),
-    MongooseModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.getOrThrow<string>('database.host'),
-        autoIndex: true,
-      }),
-      imports: [ConfigModule],
-      inject: [ConfigService],
-    }),
+    DatabaseModule,
+    EnvConfigModule.forRoot(),
     JwtModule,
     UsersModule,
     SubjectsModule,
