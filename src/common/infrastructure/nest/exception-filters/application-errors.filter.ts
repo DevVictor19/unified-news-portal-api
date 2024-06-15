994f7fa866a7fd57b051837e498525fd3b14a0c8
DevVictor19 <antoniovictor12@live.com>
2024-06-15 15:38:33 -0300

    feat: add application exception-filter

---
+++ src/common/infrastructure/nest/exception-filters/application-errors.filter.ts
@@ -0,0 +1,20 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { FastifyReply } from 'fastify';
+
+import { ApplicationError } from '@/common/application/errors/application-errors';
+
+@Catch(ApplicationError)
+export class ApplicationErrorsFilter implements ExceptionFilter {
+  catch(exception: ApplicationError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<FastifyReply>();
+
+    const errorResponse = {
+      statusCode: exception.httpStatus,
+      message: exception.message,
+      error: exception.error,
+    };
+
+    response.status(exception.httpStatus).send(errorResponse);
+  }
+}
