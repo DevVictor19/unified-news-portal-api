e888eb1027fc454217ad049dab1f3238b51797a7
DevVictor19 <antoniovictor12@live.com>
2024-06-17 01:18:11 -0300

    feat: remove redundant tests

--- src/modules/users/infrastructure/database/repositories/mongo/__test__/integration/users-mongo-repository.int-spec.ts
+++ src/modules/users/infrastructure/database/repositories/mongo/__test__/integration/users-mongo-repository.int-spec.ts
@@ -7,7 +7,6 @@ import { Connection } from 'mongoose';
 import {
   UserMongoEntity,
   UserMongoModel,
-  UserMongoSchema,
 } from '../../../../models/mongo/users-mongo.model';
 import { UsersMongoRepository } from '../../users-mongo.repository';
 
@@ -50,70 +49,6 @@ describe('UsersMongoRepository integration tests', () => {
   afterAll(async () => {
     await connection.collection(collectionName).deleteMany();
     app.close();
-  });
-
-  describe('insert', () => {
-    it('Should insert a entity', async () => {
-      await sut.insert(entity);
-
-      const insertedEntity = await connection
-        .model(getModelToken(UserMongoEntity.name), UserMongoSchema)
-        .findById(entity.id);
-
-      expect(insertedEntity).toBeTruthy();
-      expect(insertedEntity?._id).toBe(entity.id);
-    });
-  });
-
-  describe('findAll', () => {
-    it('Should find all entities', async () => {
-      await sut.insert(entity);
-
-      const entities = await sut.findAll();
-
-      expect(entities.length).toBeGreaterThan(0);
-    });
-  });
-
-  describe('findById', () => {
-    it('Should find a entity by id', async () => {
-      await sut.insert(entity);
-
-      const insertedEntity = await sut.findById(entity.id);
-
-      expect(insertedEntity).toBeTruthy();
-      expect(insertedEntity?.id).toBe(entity.id);
-    });
-  });
-
-  describe('update', () => {
-    it('Should update a entity', async () => {
-      await sut.insert(entity);
-
-      entity.name = 'New name';
-
-      await sut.update(entity.id, entity);
-
-      const updatedEntity = (await connection
-        .model(getModelToken(UserMongoEntity.name), UserMongoSchema)
-        .findById(entity.id)) as UserMongoEntity;
-
-      expect(updatedEntity.name).toBe(entity.name);
-    });
-  });
-
-  describe('delete', () => {
-    it('Should delete a entity', async () => {
-      await sut.insert(entity);
-
-      await sut.delete(entity.id);
-
-      const insertedEntity = await connection
-        .model(getModelToken(UserMongoEntity.name), UserMongoSchema)
-        .findById(entity.id);
-
-      expect(insertedEntity).toBeNull();
-    });
   });
 
   describe('findByEmail', () => {
